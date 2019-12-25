a2488ff2c8c6f3302abf940c2dcbcf53a5699274
dangolbeeker <scottb305@outlook.com>
2019-12-25 18:19:40 -0500

    Modified muteable variable

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,12 @@
 val name: String = "Nate"
+var greeting: String = "hello"
 fun main() {
 
-
+    println(greeting)
     println(name)
+
+    greeting = "hi"
+    println(greeting)
+    println(name)
+
 }
