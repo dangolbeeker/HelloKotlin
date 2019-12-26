f43753041806e39504b5ae26d3da87b7e941c90c
dangolbeeker <scottb305@outlook.com>
2019-12-25 20:03:30 -0500

    control flow IF Statement

--- src/Main.kt
+++ src/Main.kt
@@ -2,11 +2,7 @@ val name: String = "Nate"
 var greeting: String? = null
 
 fun main() {
-    greeting = "Hello"
-    when(greeting){
-        null -> println("Hi")
-        else -> println(greeting)
-    }
-
+    val greetingToPrint = if(greeting != null) greeting else "Hi"
+    println(greetingToPrint)
         println(name)
 }
