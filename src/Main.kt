9a57a3fee2daf8fc6e92d7353a3e3e8d0b2cf11f
dangolbeeker <scottb305@outlook.com>
2019-12-26 20:18:31 -0500

    Collections and Iterations

--- src/Main.kt
+++ src/Main.kt
@@ -1,21 +1,8 @@
-// Basic function
-fun sayHello(itemToGreet:String) {
-    // val msg = "Hello " + itemToGreet
-    val msg = "Hello $itemToGreet"
-    println(msg)
-}
-// Single expression function using a String Template
-fun sayHellov2(itemToGreet:String) = println("Hello $itemToGreet")
+fun sayHello(greeting:String, itemToGreet:String) = println("$greeting $itemToGreet")
 
-// Single expression function using multiple parameters as a String Template
-fun sayHellov3(greeting:String, itemToGreet:String) = println("$greeting $itemToGreet")
-
-// Default main function
 fun main() {
-    sayHello(itemToGreet = "Kotlin")
-    sayHello(itemToGreet = "World")
-    sayHellov2(itemToGreet = "Kotlin")
-    sayHellov2(itemToGreet = "World")
-    sayHellov3(greeting = "Hey", itemToGreet = "Kotlin")
-    sayHellov3(greeting = "Hello", itemToGreet = "World")
+    val interestingThings = arrayOf("Kotlin", "Programming", "Comic Books")
+    println(interestingThings.size)
+    println(interestingThings[0])
+    print(interestingThings.get(0))
 }
