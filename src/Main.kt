93687614d7d2ce90d0d83651be851828f654553b
dangolbeeker <scottb305@outlook.com>
2019-12-26 20:04:51 -0500

    function params

--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,21 @@
-fun getGreeting() = "Hello Kotlin"
-
-fun sayHello() {
-    println(getGreeting())
+// Basic function
+fun sayHello(itemToGreet:String) {
+    // val msg = "Hello " + itemToGreet
+    val msg = "Hello $itemToGreet"
+    println(msg)
 }
+// Single expression function using a String Template
+fun sayHellov2(itemToGreet:String) = println("Hello $itemToGreet")
 
+// Single expression function using multiple parameters as a String Template
+fun sayHellov3(greeting:String, itemToGreet:String) = println("$greeting $itemToGreet")
+
+// Default main function
 fun main() {
-    println("Hello World")
-  println(getGreeting())
-   sayHello()
+    sayHello(itemToGreet = "Kotlin")
+    sayHello(itemToGreet = "World")
+    sayHellov2(itemToGreet = "Kotlin")
+    sayHellov2(itemToGreet = "World")
+    sayHellov3(greeting = "Hey", itemToGreet = "Kotlin")
+    sayHellov3(greeting = "Hello", itemToGreet = "World")
 }
