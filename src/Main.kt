ab76894f6cc90fdb8f3c0702838eae53c83258c4
dangolbeeker <scottb305@outlook.com>
2019-12-27 17:26:03 -0500

    instance of class

--- src/Main.kt
+++ src/Main.kt
@@ -15,19 +15,6 @@ fun greetPerson(greeting: String = "Default", name: String = "Default") = printl
 
 
 fun main() {
-    val interestingThings = arrayOf("Kotlin", "Programming", "Java")
-    val interestingThings2 = listOf("Kotlin", "Programming", "Java")
-    sayHello2("Hallo", *interestingThings)
-    println("=========================================================================")
-    sayHello2("Hej", "Kotlin", "Java", "C#")
-    println("=========================================================================")
-    sayHello1("Hej", interestingThings2)
-    println("=========================================================================")
-    //================================================================================
-    //Named Arguments
-    greetPerson(greeting = "Hi", name = "Scott") // or switch
-    greetPerson(name = "Scott", greeting = "Hi") // or only one argument
-    greetPerson(name = "Scott") // or none
-    greetPerson()
-    sayHello2(greeting = "Hallo", itemsToGreet = *interestingThings)
+//    New Class instanance
+    val person = Person()
 }
