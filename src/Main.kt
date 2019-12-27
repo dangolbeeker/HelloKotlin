cca8a63e42c9f7c040b9f5f573f856d063d7692f
dangolbeeker <scottb305@outlook.com>
2019-12-26 23:39:43 -0500

    named arguments

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,33 @@
-fun sayHello(greeting:String, itemToGreet:String) = println("$greeting $itemToGreet")
+fun sayHello1(greeting:String, itemsToGreet: List<String>) {
+    itemsToGreet.forEach { itemToGreet ->
+        println("$greeting $itemToGreet")
+    }
+}
+
+fun sayHello2(greeting: String, vararg itemsToGreet: String) {
+    itemsToGreet.forEach { itemToGreet ->
+        println("$greeting $itemToGreet")
+    }
+}
+//====================================================================================
+//Named Arguments
+fun greetPerson(greeting: String = "Default", name: String = "Default") = println("$greeting $name")
+
 
 fun main() {
-    val interestingThings = listOf("Kotlin", "Programming", "Comic Books")
-
-    val map = mapOf(1 to "a", 2 to "b", 3 to "c")
-    map.forEach { (key, value) -> println("$key -> $value") }
-
+    val interestingThings = arrayOf("Kotlin", "Programming", "Java")
+    val interestingThings2 = listOf("Kotlin", "Programming", "Java")
+    sayHello2("Hallo", *interestingThings)
+    println("=========================================================================")
+    sayHello2("Hej", "Kotlin", "Java", "C#")
+    println("=========================================================================")
+    sayHello1("Hej", interestingThings2)
+    println("=========================================================================")
+    //================================================================================
+    //Named Arguments
+    greetPerson(greeting = "Hi", name = "Scott") // or switch
+    greetPerson(name = "Scott", greeting = "Hi") // or only one argument
+    greetPerson(name = "Scott") // or none
+    greetPerson()
+    sayHello2(greeting = "Hallo", itemsToGreet = *interestingThings)
 }
