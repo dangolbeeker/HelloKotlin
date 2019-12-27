5dcbb87ba82d4a85f8ed4726436ebb37a44dd490
dangolbeeker <scottb305@outlook.com>
2019-12-26 20:47:44 -0500

    Map key value

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,9 @@
 fun sayHello(greeting:String, itemToGreet:String) = println("$greeting $itemToGreet")
 
 fun main() {
-    val interestingThings = arrayOf("Kotlin", "Programming", "Comic Books")
-    println(interestingThings.size)
-    println(interestingThings[0])
-    print(interestingThings.get(0))
+    val interestingThings = listOf("Kotlin", "Programming", "Comic Books")
+
+    val map = mapOf(1 to "a", 2 to "b", 3 to "c")
+    map.forEach { (key, value) -> println("$key -> $value") }
+
 }
