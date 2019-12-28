e530a569065646b3007cadfe9f7bdb4c8a04c8fb
dangolbeeker <scottb305@outlook.com>
2019-12-27 19:00:14 -0500

    interface properties/implementing multiple interfaces

--- src/PersonInfoProvider.kt
+++ src/PersonInfoProvider.kt
@@ -1,16 +1,50 @@
-interface PersonInfoProvider{
-    fun printInfo(person: Person)
+fun main()
+{
+    val provider = BasicInfoProvider()
+    provider.printInfo(Person())
+    provider.getSessionId()
+    checkTypes(provider)
+    println("""""""""""""""""""""""""""""""""""""""""""""""""""""")
 }
 
-
-class BasicInfoProvider : PersonInfoProvider {
-    override  fun printInfo(person: Person) {
-            println("printInfo")
+interface PersonInfoProvider
+{
+    val providerInfo: String
+    fun printInfo(person: Person)
+    {
+        println(providerInfo)
+        person.printInfo()
     }
 }
 
-fun main() {
-val provider = BasicInfoProvider()
+interface SessionInfoProvider
+{
+    fun getSessionId(): String
+}
 
-    provider.printInfo(Person())
+// add to interfaces to a class
+open class BasicInfoProvider : PersonInfoProvider, SessionInfoProvider
+{
+    override fun getSessionId(): String
+    {
+        return "SessionId"
+    }
+
+    override val providerInfo: String
+        get() = "udemy.BasicInfoProvider"
+}
+
+
+// checks which types
+fun checkTypes(infoProvider: PersonInfoProvider)
+{
+    if (infoProvider !is SessionInfoProvider)
+    {
+        println("not a session info provider")
+    } else
+    {
+        println("is a session info provider")
+        //(infoProvider as udemy.SessionInfoProvider).getSessionId() // or
+        infoProvider.getSessionId()
+    }
 }
